64bb0b4574ba5f4a64a7e9d95f5adcc27cd1dd5c
fabian <ing.ospinagonzalez@gmail.com>
2019-07-18 16:01:58 -0500

    inicio

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.get('/', function(req , res) {
+    res.send('Hello World!')
+});
+
+
+app.listen(3500, function () {
+    console.log('se inició el servidor en port 3500');
+})
