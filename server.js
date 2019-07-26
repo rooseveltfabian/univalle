30695fac66d13a394edee2fbebf0b709cdd76891
fabian <ing.ospinagonzalez@gmail.com>
2019-07-26 08:42:16 -0500

    se agregan los cors

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 //Uso de Body Parser Para manejo de la informacion que viaja por las peticiones http al servidor
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
+
 //Importamos la libreria o modulo router mandandole como parametro la instancia de servidor express
 require('./api/router.js')(app);
 //Iniciamos el servidor escuchando por un puerto  
